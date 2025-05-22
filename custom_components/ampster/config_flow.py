73f3bb20b0a153a643eaca08b10c9e2ae8d5b49e
Alex Howarth <alex.howarth@gmail.com>
2025-05-22 09:54:06 -0400

    Fix: sensor state length, async listener, and config flow deprecation warnings

--- custom_components/ampster/config_flow.py
+++ custom_components/ampster/config_flow.py
@@ -63,8 +63,12 @@ class AmpsterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return AmpsterOptionsFlowHandler(config_entry)
 
 class AmpsterOptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
+    def __init__(self, entry):
+        self._entry = entry
+
+    @property
+    def config_entry(self):
+        return self._entry
 
     async def async_step_init(self, user_input=None):
         errors = {}

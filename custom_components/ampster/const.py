19815676b6ac96d4ac719383420fe55827be8c1d
Alex Howarth <alex.howarth@gmail.com>
2025-05-22 15:12:34 -0400

    fix

--- custom_components/ampster/const.py
+++ custom_components/ampster/const.py
@@ -6,5 +6,8 @@ DEFAULT_COUNTRY = "NL"
 DEFAULT_MINUTE = 5
 DEFAULT_BASE_URL = "https://ampster.s3.us-east-1.amazonaws.com/electricity_prices/"
 
+# For backward compatibility, provide BASE_URL as an alias for DEFAULT_BASE_URL
+BASE_URL = DEFAULT_BASE_URL
+
 # Supported countries
 SUPPORTED_COUNTRIES = ["NL", "FR", "BE", "AT"]

7d00d3771b0d29a83440b3a0c63d9c659418cd91
Alex Howarth <alex.howarth@gmail.com>
2025-05-27 13:05:31 -0400

    add a clean sample function and test for adding a sensor (42)

--- custom_components/ampster/sensor.py
+++ custom_components/ampster/sensor.py
@@ -19,7 +19,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
     
     # Add the custom hoarding periods remaining sensor
     entities.append(AmpsterHoardingPeriodsRemainingSensor(hass, coordinator)) # Pass coordinator
-    
+    entities.append(AmpsterStatic42Sensor(hass, coordinator))
+
     async_add_entities(entities)
 
 class AmpsterSensor(SensorEntity):
@@ -96,5 +97,34 @@ class AmpsterHoardingPeriodsRemainingSensor(SensorEntity):
         # Make sure to pass hass to the calculation function if it needs it
         # and potentially other data from the coordinator if required
         self._attr_native_value = await calculate_hoarding_periods_remaining(self.hass)
+
+class AmpsterStatic42Sensor(SensorEntity):
+    _attr_name = "Ampster Static 42"
+    _attr_unique_id = "ampster_static_42"
+    _attr_should_poll = False
+
+    def __init__(self, hass, coordinator):
+        self.hass = hass
+        self.coordinator = coordinator
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, coordinator.country_prefix)},
+            "name": "Ampster",
+            "manufacturer": "Ampster",
+            "entry_type": "service",
+        }
+        self._attr_native_value = 42
+
+    async def async_added_to_hass(self) -> None:
+        self.async_on_remove(
+            self.coordinator.async_add_listener(self.async_write_ha_state)
+        )
+
+    @property
+    def available(self) -> bool:
+        return self.coordinator.last_update_success
+
+    async def async_update(self):
+        # In the future, you can access other states via self.hass.states.get(...)
+        self._attr_native_value = 42
 
 # To disable exposing sensors, remove or comment out this file and its setup in __init__.py
